babe8828cf3c72c135b662524e411a0a6c6f8af7
Deivide Maciel <31144383+deivomaciel@users.noreply.github.com>
2024-11-04 19:17:13 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
-
-echo "hello, world!";
+    header('Content-Type: application/json');
+    $requestMethod = $_SERVER['REQUEST_METHOD'];
+    $requestUri = $_SERVER['REQUEST_URI'];
+    
+    switch ($requestMethod) {
+        case 'GET':
+            if (strpos($requestUri, '/') !== false) {
+               echo 'teste 3';
+            }
+            break;
+        default:
+            echo json_encode(['message' => 'Método não suportado']);
+            break;
+    }
+?>
